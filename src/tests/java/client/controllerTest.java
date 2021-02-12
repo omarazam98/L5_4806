cafb107f448176ae0f94a9ec982e78b96dbeadf4
omarazam98 <umarazam98@gmail.com>
2021-02-12 02:59:04 -0500

    configured travis ci

---
+++ src/tests/java/client/controllerTest.java
@@ -0,0 +1,22 @@
+package client;
+
+import client.web.AddressBookController;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class controllerTest {
+
+    @Autowired
+    private AddressBookController controller;
+
+    @Test
+    public void contexLoads() throws Exception {
+        Assertions.assertThat(controller).isNotNull();
+    }
+}
